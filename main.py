7ff0db6a41f202f9c25889633282b3b5cf539faa
Ananda Rama <ananda.mulya09@gmail.com>
2025-08-01 08:28:46 +0700

    little update

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import joblib
 from sklearn.preprocessing import OrdinalEncoder
 from scipy.stats import chi2_contingency
